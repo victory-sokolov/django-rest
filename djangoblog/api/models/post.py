1163f4389908a8ae1c93abb1f7962d6ad3404d64
Viktor Sokolov <viktorsokolov.and@gmail.com>
2022-04-11 21:58:36 +0300

    add update_at field

--- djangoblog/api/models/post.py
+++ djangoblog/api/models/post.py
@@ -5,14 +5,15 @@ from djangoblog.api.models.author import Author
 from djangoblog.api.models.category import Category
 from ckeditor.fields import RichTextField
 
+from djangoblog.base import TimeStampedModel
 
-class Post(models.Model):
+
+class Post(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     title = models.CharField(max_length=200)
     body = RichTextField(blank=True, null=True)
     category = models.ManyToManyField(Category, max_length=100)
     author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "posts"
