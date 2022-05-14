ddc28a743ffebb876ecaa170c08c0d3c828151d7
Viktor Sokolov <viktorsokolov.and@gmail.com>
2022-05-14 21:12:15 +0300

    add permissions

--- djangoblog/admin.py
+++ djangoblog/admin.py
@@ -3,13 +3,47 @@ from djangoblog.api.models.post import Post
 from djangoblog.models import UserProfile
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "user", "likes")
     list_filter = ("created_at",)
     search_fields = ["title"]
+    exclude = ("likes", "views", "favorites")
+    ordering = ("-user", "title")
 
 
-models = [UserProfile]
+@admin.register(UserProfile)
+class UserAdmin(admin.ModelAdmin):
 
-admin.site.register(models)
-admin.site.register(Post, PostAdmin)
+    list_display = ("email", "name", "get_total_posts")
+    fieldsets = (
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "email",
+                    "name",
+                    "password",
+                    "profile_picture",
+                )
+            },
+        ),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    def get_total_posts(self, obj):
+        return obj.post_set.count()
+
+    get_total_posts.short_description = "Total posts"
---
+++ djangoblog/utils/utils.py
@@ -0,0 +1,2 @@
+def slugify(content: str) -> str:
+    return content.lower().replace(" ", "-")
