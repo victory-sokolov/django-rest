4aeca0fbf152bd5924c34658600d6d50f5302a21
Viktor Sokolov <viktorsokolov.and@gmail.com>
2022-05-16 18:27:32 +0300

    seed users and posts command

--- djangoblog/management/commands/post.py
+++ djangoblog/management/commands/post.py
@@ -5,13 +5,15 @@ from django.core.management.base import BaseCommand, CommandParser
 from faker import Faker
 from djangoblog.api.models.post import Post
 from djangoblog.models import UserProfile
+from djangoblog.utils.utils import slugify
 
 
 class Command(BaseCommand):
     help = "Seed database for testing and development"
 
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("--amount", type=str, help="amount of posts to generate")
+        parser.add_argument("gen", type=str)
+        parser.add_argument("-a", "--amount", type=int, help="amount to generate")
 
     def handle(self, *args, **options):
         logging.info("Adding new posts")
@@ -31,10 +33,12 @@ def run_seed(amount: int):
     users = UserProfile.objects.all()
     posts = []
     for _ in range(amount):
+        title = f"{fake.company()} {fake.country()}"
         post = Post(
             user=random.choice(users),
-            title=f"{fake.company()} {fake.country()}",
+            title=title,
             content=fake.text(),
+            slug=slugify(title),
         )
         posts.append(post)
 
---
+++ djangoblog/management/commands/user.py
@@ -0,0 +1,35 @@
+import logging
+
+from django.core.management.base import BaseCommand, CommandParser
+from faker import Faker
+from djangoblog.models import UserProfile
+from django.contrib.auth.hashers import make_password
+
+
+class Command(BaseCommand):
+    help = "Seed database witht users"
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "-a", "--amount", type=int, help="amount of users to generate"
+        )
+
+    def handle(self, *args, **options):
+        logging.info("Creating new users")
+        amount = int(options["amount"])
+        run_seed(amount)
+        self.stdout.write(f"Added {amount} users")
+
+
+def run_seed(amount: int):
+    fake = Faker()
+    user_list = []
+    for _ in range(amount):
+        user = UserProfile(
+            name=fake.name(),
+            email=fake.email(),
+            password=make_password(fake.password()),
+        )
+        user_list.append(user)
+
+    UserProfile.objects.bulk_create(user_list)
