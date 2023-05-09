6f0530a2047cf393f7b1776cc7e4446f583c1b1a
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-05-09 23:07:57 +0300

    update

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -28,8 +28,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
 
 urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
 
