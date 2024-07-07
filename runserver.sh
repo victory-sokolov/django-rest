4e604b76dd4f514c406b30d14fa8aefc098cf255
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-07-07 10:16:06 +0300

    update

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 
 while true; do
   echo "Re-starting Django runserver"
-  poetry run python manage.py runserver
+  DJANGO_ENV=local poetry run python manage.py runsslserver 0.0.0.0:8000
   sleep 1
 done
