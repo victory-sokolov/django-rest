9d132f3243f806bfbf95773bec3a57d6e7fee9eb
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-07-08 17:27:30 +0300

    Remove init and use setUpTestData

--- djangoblog/tests/test_swagger.py
+++ djangoblog/tests/test_swagger.py
@@ -5,11 +5,11 @@ from django.test.client import Client
 
 
 class TestSwaggerSchema(TestCase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.client = Client()
-        self.response_v1 = self.client.get("/api/v1/schema.json")
-        self.content_v1 = json.loads(self.response_v1.content.decode("utf-8"))
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.response_v1 = cls.client.get("/api/v1/schema.json")
+        cls.content_v1 = json.loads(cls.response_v1.content.decode("utf-8"))
 
     def test_swagger_schema(self):
         self.assertEqual(self.response_v1.status_code, 200)
