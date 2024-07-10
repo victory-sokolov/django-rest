4290e2f45041d3fb731a9d3fce9f4c63e13169b5
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-07-10 09:55:21 +0300

    Fix actions
    fix actions
    
    update

--- pg-bootstrap.sh
+++ pg-bootstrap.sh
@@ -6,7 +6,7 @@ set -u
 function create_user_and_database() {
 	local database=$1
 	echo "Creating database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --host "$POSTGRES_HOST" <<-EOSQL
+	psql -v ON_ERROR_STOP=1 --host "$POSTGRES_HOST" --port "$PGPORT" <<-EOSQL
 	    CREATE USER $database;
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
