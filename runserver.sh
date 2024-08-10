d08dcc521441baeab25bcc51928f46648d2013d8
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-08-10 21:57:23 +0300

    set gunicorn worker count

--- runserver.sh
+++ runserver.sh
@@ -3,4 +3,4 @@
 # Getting static files for Admin panel hosting!
 DJANGO_ENV=production python manage.py collectstatic --noinput
 DJANGO_ENV=production python manage.py migrate
-DJANGO_ENV=production gunicorn djangoblog.wsgi:application --bind 0.0.0.0:"${PORT}"
+DJANGO_ENV=production gunicorn djangoblog.wsgi:application --bind 0.0.0.0:"${PORT}" --workers 1
