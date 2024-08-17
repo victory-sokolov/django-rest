72991e02673ba9506c309892f4dcaf88b211da00
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-08-17 10:24:02 +0300

    add timeout and loglevel to gunicorn

--- runserver.sh
+++ runserver.sh
@@ -3,4 +3,4 @@
 # Getting static files for Admin panel hosting!
 DJANGO_ENV=production python manage.py collectstatic --noinput
 DJANGO_ENV=production python manage.py migrate
-DJANGO_ENV=production gunicorn djangoblog.wsgi:application --bind 0.0.0.0:"${PORT}" --workers 1
+DJANGO_ENV=production gunicorn djangoblog.wsgi:application --bind 0.0.0.0:"${PORT}" --workers 1 --timeout 90 --log-level debug
