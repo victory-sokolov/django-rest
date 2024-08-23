44628af22507ab6d76283849379aa6103e3d26ff
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-08-23 21:02:33 +0300

    update

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-# Getting static files for Admin panel hosting!
 DJANGO_ENV=production python manage.py collectstatic --noinput
 DJANGO_ENV=production python manage.py migrate
 DJANGO_ENV=production gunicorn djangoblog.wsgi:application --config gunicorn.py --bind 0.0.0.0:"${PORT:-80}"
