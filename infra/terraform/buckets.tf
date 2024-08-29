e2511b0ef1636510cb02a091fa4891bdf24f76da
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-08-29 16:55:29 +0300

    add uniform_bucket_level_access

--- infra/terraform/buckets.tf
+++ infra/terraform/buckets.tf
@@ -1,10 +1,11 @@
 # Bucket configuration
 resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
-  location      = var.location
-  project       = var.project_id
-  force_destroy = false
-  storage_class = "NEARLINE"
+  name                        = var.bucket_name
+  location                    = var.location
+  project                     = var.project_id
+  uniform_bucket_level_access = true
+  force_destroy               = false
+  storage_class               = "NEARLINE"
 
   lifecycle {
     prevent_destroy = false
