dc6326a7090a8972845277d7aee8252d2d7d0651
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-09-02 19:46:51 +0300

    ignore sppecify statics

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
-DJANGO_ENV=production python manage.py collectstatic --noinput
+DJANGO_ENV=production python manage.py compress --force
+DJANGO_ENV=production python manage.py collectstatic \
+    --noinput \
+    -i admin \
+    -i tagify \
+    -i django_extensions \
+    -i rest_framework
 DJANGO_ENV=production python manage.py migrate
 DJANGO_ENV=production gunicorn djangoblog.wsgi:application --config gunicorn.py --bind 0.0.0.0:"${PORT:-80}"
