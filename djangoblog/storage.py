d5e706e6f95c9314e2fe9d8c26c26f8a49fdbd1a
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-09-11 13:30:56 +0300

    update storage settings

--- djangoblog/storage.py
+++ djangoblog/storage.py
@@ -1,10 +1,18 @@
-from typing import Any, Optional
+import json
+from typing import Any
 
+from compressor.storage import CompressorFileStorage
 from django.core.files.storage import storages
-from google.cloud.exceptions import NotFound
-from google.cloud.storage.retry import DEFAULT_RETRY
 from storages.backends.gcloud import GoogleCloudStorage
-from storages.utils import clean_name
+
+
+class CustomOfflineManifestStorage(CompressorFileStorage):
+    def read_manifest(self):
+        try:
+            with self.open(self.manifest_name) as manifest_file:
+                return json.load(manifest_file)
+        except IOError:
+            return {}
 
 
 class CachedGCloudStorage(GoogleCloudStorage):
@@ -13,27 +21,10 @@ class CachedGCloudStorage(GoogleCloudStorage):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.local_storage = storages.create_storage(
-            {"BACKEND": "compressor.storage.DefaultOfflineManifestStorage"},
+            {"BACKEND": "compressor.storage.CompressorFileStorage"},
         )
 
     def save(self, name: str, content: str) -> str:
-        self.local_storage.save(name, content)
+        self.local_storage._save(name, content)
         super().save(name, self.local_storage._open(name))
         return name
-
-    def exists(self, name: Optional[str]) -> bool:
-        print("Name ->", name)
-        if name:
-            try:
-                self.bucket.delete_blob(name, retry=DEFAULT_RETRY)
-            except NotFound:
-                pass
-        else:
-            try:
-                self.client.get_bucket(self.bucket)
-                return True
-            except NotFound:
-                return False
-
-        name = self._normalize_name(clean_name(name))
-        return bool(self.bucket.get_blob(name))
