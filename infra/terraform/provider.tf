6c48920305a6b806c2b12ae907d11b5fe77bb533
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-09-21 12:59:54 +0300

    add credentials

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -7,7 +7,8 @@ provider "google" {
 }
 
 provider "google-beta" {
-  project = var.project_id
-  region  = var.gcp_region
-  zone    = var.gcp_zone
+  credentials = file("gcp-creds.json")
+  project     = var.project_id
+  region      = var.gcp_region
+  zone        = var.gcp_zone
 }
