e6d7519b015d85d53e630bd31f476971273f2318
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-09-26 17:52:38 +0300

    add types for PostException

--- djangoblog/api/error.py
+++ djangoblog/api/error.py
@@ -1,8 +1,12 @@
+from typing import Any
+
 from rest_framework.exceptions import ValidationError
 
 
 class PostException(Exception):
     @staticmethod
-    def validation_error(error, field, **kwargs) -> ValidationError:
+    def validation_error(
+        error: str | list, field: str, **kwargs: Any
+    ) -> ValidationError:
         data = {"details": error, "summary": f"Field {field} {error[0]}"}
         return ValidationError(data)
