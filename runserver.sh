ce2a57d9cf63ce3ac083bbf914b84b2f1ebf3f92
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-09-27 14:55:31 +0300

    ignore silk package

--- runserver.sh
+++ runserver.sh
@@ -2,7 +2,7 @@
 
 echo "Using $DJANGO_ENV environment"
 
-DJANGO_ENV=$DJANGO_ENV python manage.py collectstatic --noinput -i silk
+DJANGO_ENV=$DJANGO_ENV python manage.py collectstatic --noinput -i silk/*
 DJANGO_ENV=$DJANGO_ENV python manage.py compress --force
 DJANGO_ENV=$DJANGO_ENV python manage.py migrate
 DJANGO_ENV=$DJANGO_ENV gunicorn djangoblog.wsgi:application --config gunicorn.py --bind 0.0.0.0:"${PORT:-80}"
