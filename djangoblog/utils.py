3dd9e7a3e9c4c886a0980e9da44059f3aa8996c1
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-09-28 17:53:41 +0300

    update types

--- djangoblog/utils.py
+++ djangoblog/utils.py
@@ -1,6 +1,8 @@
 import inspect
 import logging
 
+from typing import Any, Callable
+
 logger = logging.getLogger(__name__)
 
 
@@ -8,10 +10,10 @@ def slugify(content: str) -> str:
     return content.lower().replace(" ", "-").replace(",", "")
 
 
-def enable_args_debugger(func):
+def enable_args_debugger(func: Callable) -> Callable:
     """Decorator to print function call details and parameters names and values."""
 
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: Any, **kwargs: Any) -> Callable:
         func_args = inspect.signature(func).bind(*args, **kwargs).arguments
         func_args_str = ", ".join(
             map("{0[0]} = {0[1]!r}".format, func_args.items()),
