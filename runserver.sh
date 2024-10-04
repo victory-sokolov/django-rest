0e0fc52274362d970a404aa500b8ff57b28325a7
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-10-04 20:15:37 +0300

    run gunciorn on production or django runserver

--- runserver.sh
+++ runserver.sh
@@ -8,4 +8,9 @@ DJANGO_ENV=$DJANGO_ENV uv run python manage.py check --settings=djangoblog.setti
 DJANGO_ENV=$DJANGO_ENV uv run python manage.py collectstatic --noinput -i silk/*
 DJANGO_ENV=$DJANGO_ENV uv run python manage.py compress --force
 DJANGO_ENV=$DJANGO_ENV uv run python manage.py migrate
-DJANGO_ENV=$DJANGO_ENV uv run gunicorn djangoblog.wsgi:application --config gunicorn.py --bind 0.0.0.0:"${PORT:-80}"
+
+if [ "$DJANGO_ENV" = "production" ]; then
+    DJANGO_ENV=$DJANGO_ENV uv run gunicorn djangoblog.wsgi:application --config gunicorn.py --bind 0.0.0.0:"${PORT:-80}"
+else
+    DJANGO_ENV=$DJANGO_ENV uv run python manage.py runserver
+fi
