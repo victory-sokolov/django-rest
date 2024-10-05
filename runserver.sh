d2cd518830737ae8cc85bc39ae992c807327dee7
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-10-05 10:42:19 +0300

    create/post and superuser for non prod env

--- runserver.sh
+++ runserver.sh
@@ -12,5 +12,9 @@ DJANGO_ENV=$DJANGO_ENV uv run python manage.py migrate
 if [ "$DJANGO_ENV" = "production" ]; then
     DJANGO_ENV=$DJANGO_ENV uv run gunicorn djangoblog.wsgi:application --config gunicorn.py --bind 0.0.0.0:"${PORT:-80}"
 else
-    DJANGO_ENV=$DJANGO_ENV uv run python manage.py runserver
+    # Create superuser
+    DJANGO_ENV=$DJANGO_ENV uv run python manage.py create_superuser --user=admin --password=superPassword12 --email=admin@gmail.com
+    # Generate posts
+    DJANGO_ENV=$DJANGO_ENV uv run manage.py create_posts --count 100
+    DJANGO_ENV=$DJANGO_ENV uv run python manage.py runserver 0.0.0.0:"${PORT:-80}"
 fi
