a5086e1dea9b3f30b1bf1d369792629f84f867f6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-10-12 21:09:32 +0300

    update

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 
 echo "Using $DJANGO_ENV environment"
 
@@ -8,12 +8,13 @@ DJANGO_ENV=$DJANGO_ENV python manage.py collectstatic --noinput -i silk/*
 DJANGO_ENV=$DJANGO_ENV python manage.py compress --force
 DJANGO_ENV=$DJANGO_ENV python manage.py migrate
 
+# Create superuser
+DJANGO_ENV=$DJANGO_ENV python manage.py create_superuser --user=admin --password=superPassword12 --email=admin@gmail.com
+# Generate posts
+DJANGO_ENV=$DJANGO_ENV python manage.py create_posts --count 100
+
 if [ "$DJANGO_ENV" = "production" ]; then
     DJANGO_ENV=$DJANGO_ENV gunicorn djangoblog.wsgi:application --config gunicorn.py --bind 0.0.0.0:"${PORT:-80}"
 else
-    # Create superuser
-    DJANGO_ENV=$DJANGO_ENV python manage.py create_superuser --user=admin --password=superPassword12 --email=admin@gmail.com
-    # Generate posts
-    DJANGO_ENV=$DJANGO_ENV python manage.py create_posts --count 100
     DJANGO_ENV=$DJANGO_ENV python manage.py runserver 0.0.0.0:"${PORT:-80}"
 fi
