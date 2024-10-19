2006108be695486665994ffc14ac508bd91ea6d8
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-10-19 18:52:02 +0300

    add protocol and accept

--- gunicorn.py
+++ gunicorn.py
@@ -3,5 +3,7 @@ threads = 2
 worker_class = "gevent"
 timeout = 60
 loglevel = "info"
+protocol = "proxy"
+accept = 443
 # accesslog = "/path/to/gunicorn.access.log"
 # errorlog = "/path/to/gunicorn.error.log"
