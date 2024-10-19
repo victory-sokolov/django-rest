ffee1197ea8dc3ffd511f184c9cc8e2bc683a38c
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-10-19 17:57:18 +0300

    reuse commands from Makefile

--- runserver.sh
+++ runserver.sh
@@ -4,17 +4,18 @@ set -eo pipefail
 
 echo "Using $DJANGO_ENV environment"
 
-DJANGO_ENV=$DJANGO_ENV python manage.py collectstatic --noinput -i silk/*
+make migrate
+make collectstatic
 DJANGO_ENV=$DJANGO_ENV python manage.py compress --force
-DJANGO_ENV=$DJANGO_ENV python manage.py migrate
 
 # Create superuser
-DJANGO_ENV=$DJANGO_ENV python manage.py create_superuser --user=admin --password=superPassword12 --email=admin@gmail.com
+make create-superuser
 # Generate posts
 DJANGO_ENV=$DJANGO_ENV python manage.py create_posts --count 100
 
 if [ "$DJANGO_ENV" = "production" ]; then
-    DJANGO_ENV=$DJANGO_ENV gunicorn djangoblog.wsgi:application --config gunicorn.py --bind 0.0.0.0:"${PORT:-80}"
+    make prod
 else
+    make dev
     DJANGO_ENV=$DJANGO_ENV python manage.py runserver 0.0.0.0:"${PORT:-80}"
 fi
