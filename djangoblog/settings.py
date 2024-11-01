60dc7bfa169a2f84d94f3458f80f76a0db47986e
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-11-01 11:49:24 +0200

    after logout redirect to home

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -107,7 +107,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "login"
+LOGOUT_REDIRECT_URL = "home"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
