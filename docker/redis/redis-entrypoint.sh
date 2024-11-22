6a8e6c173bf36dffa793a867fc0713f98b2fd00f
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-11-22 17:32:32 +0200

    move redis settings from entrypoint script to redis.conf

--- docker/redis/redis-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-redis-server /redis/redis.conf \
-    --tls-port 6379 --port 0 \
-    --tls-cert-file ./tls/redis.crt \
-    --tls-key-file ./tls/redis.key \
-    --tls-ca-cert-file ./tls/ca.crt
