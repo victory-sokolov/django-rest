75fe86f95bc4e85e3808e242dcf802602fa74d2b
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-01-15 18:58:35 +0200

    revert previous changes

--- djangoblog/migrations/0001_initial.py
+++ djangoblog/migrations/0001_initial.py
@@ -8,6 +8,10 @@ class Migration(migrations.Migration):
 
     initial = True
 
+    dependencies = [
+        ('auth', '0012_alter_user_first_name_max_length'),
+    ]
+
     operations = [
         migrations.CreateModel(
             name='UserProfile',
