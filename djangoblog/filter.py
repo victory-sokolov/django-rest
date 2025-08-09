afac80a11e78a9244825b8010eedbcf59f3b9ea0
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-08-09 21:14:36 +0300

    fix excluded_paths

--- djangoblog/filter.py
+++ djangoblog/filter.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 class LogFilter(logging.Filter):
     def filter(self, record: logging.LogRecord) -> bool:
-        excluded_paths = ["/metrics", "/healthcheck"]
+        excluded_paths = ["/metrics", "/health/app"]
         message = record.getMessage()
 
         return not any(path in message for path in excluded_paths)
