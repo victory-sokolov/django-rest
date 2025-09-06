9ade5601baf17c2ef0f898bd0b5566bc15c776ec
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-09-06 16:21:36 +0300

    add task serialization using msgpack and task compression using gzip

--- djangoblog/celeryconfig.py
+++ djangoblog/celeryconfig.py
@@ -9,13 +9,19 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoblog.settings")
 class CeleryConfig:
     enable_utc = True
     timezone = "Europe/Kiev"
-    task_serializer = "json"
-    result_serializer = "json"
-    event_serializer = "json"
-    accept_content = ["json"]
-    broker_connection_retry_on_startup = (True,)
+    task_serializer = "msgpack"
+    result_serializer = "msgpack"
+    event_serializer = "msgpack"
+    accept_content = ["json", "msgpack"]
+    broker_connection_retry_on_startup = True
+    # Compress task payloads and results to reduce size in transport/result backend
+    task_compression = "gzip"
+    result_compression = "gzip"
+    task_compression_level = 6
+    result_persistent = False
     hijack_root_logger = False
-    broker_connection_max_retries = (5,)
+    broker_connection_max_retries = 5
+    worker_concurrency = 8
     imports = ("djangoblog.tasks",)
     task_default_queue = "blog"
     result_backend = settings.CELERY_RESULT_BACKEND
@@ -28,4 +34,5 @@ class CeleryConfig:
     # The SENT event indicates when a task is sent
     task_send_sent_event = True
     worker_max_tasks_per_child = 100
-    worker_max_memory_per_child = 200000  # 200 MB
+    worker_max_memory_per_child = 350000  # 350 MB
+    result_expires = 600  # 10 minutes
