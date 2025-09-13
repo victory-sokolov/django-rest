1582555c475dbf272cea12cb0edb07db0a291c32
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-09-13 17:17:11 +0300

    remove wait-for-it

--- runserver.sh
+++ runserver.sh
@@ -12,14 +12,6 @@ make UV_RUN= create-superuser
 # Generate posts
 make UV_RUN= create-posts
 
-# wait-for-it \
-#   --host="$DB_HOST" \
-#   --port="$PGPORT" \
-#   --timeout=90 \
-#   --strict
-
-# echo "Postgres ${DB_HOST}:${PGPORT} is up"
-
 echo "App is running on PORT: $PORT"
 if [ "$DJANGO_ENV" = "production" ]; then
     echo "Running with $DJANGO_ENV config"
